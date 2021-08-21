39a03e18659ccb4f2c441e8d986ac83b50394d92
Krista Marbury <laetaiustus@gmail.com>
2021-08-21 18:40:12 -0500

    created projects page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 import { Link } from "react-router-dom";
 import apples from "./images/codingk.png";
 
@@ -32,87 +33,9 @@ function App() {
           </Route>
 
           {/* <!-- Sticky Notes Focused on Recent Projects --> */}
-          <section className="projects">
-            <h1>Projects!</h1>
-
-            <div className="onesie">
-              <Link
-                to="https://kristamarbury.github.io/CodeQuiz"
-                alt="link to code quiz site"
-              >
-                <article className=" note is-clickable sticky1 pgkhw1">
-                  <div className="pin"></div>
-                  {/* <!-- move text to the center of the sticky --> */}
-                  <div className="text"></div>
-                </article>
-                <button className="lined-thick">
-                  <Link
-                    to="https://github.com/KristaMarbury/CodeQuiz"
-                    alt="link to code quiz repo"
-                  >
-                    Code Quiz Repo
-                  </Link>
-                </button>
-              </Link>
-            </div>
-            <div className="onesie">
-              <Link
-                to="https://team-one-to-rule-them-all.github.io/projectone/"
-                alt="link to project one live site"
-              >
-                <article className="note is-clickable sticky3 project1">
-                  <div className="pin"></div>
-                  <div className="text"></div>
-                </article>
-              </Link>
-              <button className="lined-thick">
-                <Link
-                  href="https://github.com/team-one-to-rule-them-all/projectone"
-                  alt="link to project one repo"
-                >
-                  Project One Repo
-                </Link>
-              </button>
-            </div>
-            <div className="onesie">
-              <Link
-                to="https://kristamarbury.github.io/WorkDayScheduler/"
-                alt="link to work day scheduler live site"
-              >
-                <article className="note is-clickable sticky0 dayplanner">
-                  <div className="pin"></div>
-                  <div className="text"></div>
-                </article>
-              </Link>
-              <button className="lined-thick">
-                <Link
-                  to="https://github.com/KristaMarbury/WorkDayScheduler"
-                  alt="link to work day scheduler repo"
-                >
-                  Workday Scheduler Repo
-                </Link>
-              </button>
-            </div>
-            <div className="onesie">
-              <Link
-                to="http://herobattle.herokuapp.com/"
-                alt="link to mothership squad goals live site"
-              >
-                <article className="note is-clickable sticky3 project2">
-                  <div className="pin"></div>
-                  <div className="text"></div>
-                </article>
-              </Link>
-              <button className="lined-thick">
-                <Link
-                  to="https://github.com/projecttwocooltoquit/mothershipsquadgoals"
-                  alt="link to mothership squad goals repo"
-                >
-                  Mothership Squad Goals Repo
-                </Link>
-              </button>
-            </div>
-          </section>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
 
           {/* <!-- Sticky Notes To Connect with Me --> */}
           <section clas="connect is-full">
---
+++ src/pages/Projects.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <main>
+      <section className="projects">
+        <h1>Projects!</h1>
+        <div className="onesie">
+          <Link
+            to="https://kristamarbury.github.io/CodeQuiz"
+            alt="link to code quiz site"
+          >
+            <article className=" note is-clickable sticky1 pgkhw1">
+              <div className="pin"></div>
+              {/* <!-- move text to the center of the sticky --> */}
+              <div className="text"></div>
+            </article>
+            <button className="lined-thick">
+              <Link
+                to="https://github.com/KristaMarbury/CodeQuiz"
+                alt="link to code quiz repo"
+              >
+                Code Quiz Repo
+              </Link>
+            </button>
+          </Link>
+        </div>
+        <div className="onesie">
+          <Link
+            to="https://team-one-to-rule-them-all.github.io/projectone/"
+            alt="link to project one live site"
+          >
+            <article className="note is-clickable sticky3 project1">
+              <div className="pin"></div>
+              <div className="text"></div>
+            </article>
+          </Link>
+          <button className="lined-thick">
+            <Link
+              href="https://github.com/team-one-to-rule-them-all/projectone"
+              alt="link to project one repo"
+            >
+              Project One Repo
+            </Link>
+          </button>
+        </div>
+        <div className="onesie">
+          <Link
+            to="https://kristamarbury.github.io/WorkDayScheduler/"
+            alt="link to work day scheduler live site"
+          >
+            <article className="note is-clickable sticky0 dayplanner">
+              <div className="pin"></div>
+              <div className="text"></div>
+            </article>
+          </Link>
+          <button className="lined-thick">
+            <Link
+              to="https://github.com/KristaMarbury/WorkDayScheduler"
+              alt="link to work day scheduler repo"
+            >
+              Workday Scheduler Repo
+            </Link>
+          </button>
+        </div>
+        <div className="onesie">
+          <Link
+            to="http://herobattle.herokuapp.com/"
+            alt="link to mothership squad goals live site"
+          >
+            <article className="note is-clickable sticky3 project2">
+              <div className="pin"></div>
+              <div className="text"></div>
+            </article>
+          </Link>
+          <button className="lined-thick">
+            <Link
+              to="https://github.com/projecttwocooltoquit/mothershipsquadgoals"
+              alt="link to mothership squad goals repo"
+            >
+              Mothership Squad Goals Repo
+            </Link>
+          </button>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
